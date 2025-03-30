6f6a89653ee9919c194a1dcd9bc454e4b54d72eb
AdityaGaikar91 <gaikaditya795@gmail.com>
2025-03-30 13:11:46 +0530

    Added comming soon page for explore

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button'
 import React from 'react'
 import Authentication from './Authentication'
+import Link from 'next/link';
 
 function Hero() {
   return (
@@ -9,8 +10,9 @@ function Hero() {
         <h2 className='font-bold text-6xl text-center'>AI Youtube Short Video Generator</h2>
         <p className='mt-4 text-2xl text-center text-gray-500'>🤖 AI generates scripts, images, and voiceovers in seconds. ⚡Create, edit, and publish engaging shorts with ease!</p>
         <div className='mt-7 gap-8 flex pointer-events-auto'>
+        <Link href="/explore">
             <Button size="lg" variant="secondary">Explore</Button>
-
+        </Link>
             <Authentication>
                 <Button size="lg">Get Started</Button>
             </Authentication>
