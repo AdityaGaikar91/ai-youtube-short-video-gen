e5c4ca7e6dcf65cac2c604059ac9d5eabfbb37a0
aditya.gaikar <gaikaditya795@gmail.com>
2026-07-10 12:42:59 +0530

    updated next.configs.mjs with remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains: ['lh3.googleusercontent.com','firebasestorage.googleapis.com']
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'firebasestorage.googleapis.com',
+            }
+        ]
     },
     transpilePackages: ['@splinetool/react-spline']
 };
